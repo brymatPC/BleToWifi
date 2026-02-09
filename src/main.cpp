3f27d3ad54d1bf16ff339105b40367ad0cc34a2b
Bryan Matthews <bmatthews@peninsulaconsulting.ca>
2026-02-09 10:26:05 -0800

    Print system status regularly

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "WifiConnection.h"
 #include "HttpExecServer.h"
 #include <utility/LedBlink.h>
+#include <utility/IntervalTimer.h>
 #include "TelnetServer.h"
 #include "UploadDataClient.h"
 #include <Preferences.h>
@@ -39,6 +40,8 @@
 //#define LOG_MASK 0x80331303
 #define LOG_MASK 0x80000003
 
+#define STATUS_TIME_INTERVAL_MS (60000)
+
 #define LED_PIN 21
 
 #define YRSHELL_ON_TELNET
@@ -62,6 +65,29 @@ BleConnection bleConnection(&dbg);
 VictronDevice victronParser;
 TempHumidityParser tempHumParser;
 
+IntervalTimer systemStatusTimer;
+esp_reset_reason_t resetReasonStartup;
+
+static char s_appName[] = "ESP32 BLE Test";
+static char s_appVersion[] = "0.9.0";
+
+static char s_resetUnknownStr[]     = "unknown";
+static char s_resetPowerOnStr[]     = "power-on";
+static char s_resetExtStr[]         = "external";
+static char s_resetSoftwareStr[]    = "Software";
+static char s_resetPanicStr[]       = "panic";
+static char s_resetIntWdogStr[]     = "interrupt watchdog";
+static char s_resetTaskWdogStr[]    = "task watchdog";
+static char s_resetOthWdogStr[]     = "other watchdog";
+static char s_resetDeepSleepStr[]   = "deep sleep";
+static char s_resetBrownOutStr[]    = "brown out";
+static char s_resetSDIOStr[]        = "sdio";
+static char s_resetUsbStr[]         = "usb";
+static char s_resetJtagStr[]        = "jtag";
+static char s_resetEFuseStr[]       = "efuse";
+static char s_resetPowerGlitchStr[] = "power glitch";
+static char s_resetCpuLockupStr[]   = "cpu lockup";
+
 void timeSyncNotification(struct timeval *tv) {
     dbg.print(__FILE__, __LINE__, 1, "Notification of a time synchronization event");
 }
@@ -78,12 +104,69 @@ void startSntp(void) {
     }
 }
 
+char *resetReasonToString(esp_reset_reason_t reason) {
+  char *result;
+  switch (reason) {
+  case ESP_RST_POWERON:
+      result = s_resetPowerOnStr;
+      break;
+  case ESP_RST_EXT:
+      result = s_resetExtStr;
+      break;
+  case ESP_RST_SW:
+      result = s_resetSoftwareStr;
+      break;
+  case ESP_RST_PANIC:
+      result = s_resetPanicStr;
+      break;
+  case ESP_RST_INT_WDT:
+      result = s_resetIntWdogStr;
+      break;
+  case ESP_RST_TASK_WDT:
+      result = s_resetTaskWdogStr;
+      break;
+  case ESP_RST_WDT:
+      result = s_resetOthWdogStr;
+      break;
+  case ESP_RST_DEEPSLEEP:
+      result = s_resetDeepSleepStr;
+      break;
+  case ESP_RST_BROWNOUT:
+      result = s_resetBrownOutStr;
+      break;
+  case ESP_RST_SDIO:
+      result = s_resetSDIOStr;
+      break;
+  case ESP_RST_USB:
+      result = s_resetUsbStr;
+      break;
+  case ESP_RST_JTAG:
+      result = s_resetJtagStr;
+      break;
+  case ESP_RST_EFUSE:
+      result = s_resetEFuseStr;
+      break;
+  case ESP_RST_PWR_GLITCH:
+      result = s_resetPowerGlitchStr;
+      break;
+  case ESP_RST_CPU_LOCKUP:
+      result = s_resetCpuLockupStr;
+      break;
+  case ESP_RST_UNKNOWN:
+  default:
+      result = s_resetUnknownStr;
+      break;
+  }
+  return result;
+}
 
 void setup(){
   unsigned httpPort = 80;
   unsigned telnetPort = 23;
   unsigned telnetLogPort = 2023;
   dbg.setMask( LOG_MASK);
+
+  resetReasonStartup = esp_reset_reason();
 
   // BAM - 20260108 - ESP32 uses a USB interface and doesn't support different baud rates
   BSerial.begin( 115200);
@@ -95,6 +178,8 @@ void setup(){
   } else {
     dbg.print( __FILE__, __LINE__, 1, "setup_Mounted_file_system:");
   }
+
+  systemStatusTimer.setInterval(STATUS_TIME_INTERVAL_MS);
 
   #ifndef HAS_LED_STRIP
   onBoardLed.setLedPin( LED_PIN);
@@ -153,6 +238,14 @@ void setup(){
 
 void loop() {
   Sliceable::sliceAll( );
+
+  if(systemStatusTimer.isNextInterval()) {
+    dbg.print(__FILE__, __LINE__, 1, "System Status Only, not a reboot!");
+    dbg.print(__FILE__, __LINE__, 1, s_appName, "s_appName");
+    dbg.print(__FILE__, __LINE__, 1, s_appVersion, "s_appVersion");
+    dbg.print(__FILE__, __LINE__, 1, resetReasonStartup, resetReasonToString(resetReasonStartup), "Reset Reason - resetReasonStartup, resetReasonStartupStr");
+  }
+
   bool telnetSpaceAvailable = telnetLogServer.spaceAvailable( 32);
   bool serialSpaceAvailable = (Serial.availableForWrite() > 32);
   if( dbg.valueAvailable() && (telnetSpaceAvailable || serialSpaceAvailable)) {
